a67fbd2dfee62bed71cf30a21d34aa22ba9f41e8
党雪云 <dangxy@zhiruifin.com>
2024-08-16 10:25:45 +0800

    bloomberg资讯主体识别

---
+++ test0814NER_PROMPT/extract_df.py
@@ -0,0 +1,74 @@
+import logging
+
+import pandas as pd
+import openai
+from dotenv import load_dotenv, find_dotenv
+from openai import OpenAI
+
+# 加载环境变量
+load_dotenv(find_dotenv())
+
+# 设置你的 OpenAI API 密钥
+# openai.api_key = 'your-api-key-here'
+openai = OpenAI()
+
+# 配置日志记录
+logging.basicConfig(filename='company_names.log', level=logging.INFO,
+                    format='%(asctime)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+
+# 定义打印分割线的函数
+def print_dashes(num=100):
+    print()
+    print('-' * num)
+    print()
+
+
+def extract_company_names(text):
+    # 定义提取公司名称的任务
+    prompt = f"请提取以下文本中的公司名称：{text} 只输出文本中的公司名称，不要输出其他不相关的内容。如果文本中有多个公司名称，依次输出所有的公司名称，并用“、”分隔。如果文本中没有公司名称，则输出""，不要输出其他内容。"
+
+    # 调用 GPT-4 模型
+    response = openai.chat.completions.create(
+      model="gpt-4o-mini",
+      messages=[
+            {"role": "system", "content": "你是一个专业的数据标注人员。"},
+            {"role": "user", "content": prompt}
+        ]
+    )
+
+    # 获取 gpt-4o-mini 模型的回答
+    company_names = response.choices[0].message.content
+    # res = '、'.join(company_names.split('\n'))
+
+    # 打印原始文本
+    print(f"原始文本：\n{text}")
+    logging.info(f"原始文本：\n{text}")
+
+    # 打印提取出的公司名称
+    print("提取出的公司名称如下：")
+    # print(res)
+    print(company_names)
+    print_dashes()
+
+    # 将公司名称写入日志文件
+    logging.info("提取出的公司名称如下：\n%s", company_names)
+    logging.info(f"{'-' * 100}")
+    return company_names
+
+
+# 1. 读取CSV文件
+# file_path = './data/0816_data_bloomberg.csv'
+file_path = './data/data_bloomberg20240814155117.csv'
+df = pd.read_csv(file_path)
+
+# 5. 应用函数到CONTENT列
+df['公司实体'] = df['content'].apply(extract_company_names)
+
+# 6. 保存结果到新的CSV文件
+# output_path = './data/output/result_0816_data_bloomberg.csv'
+output_path = './data/output/result_data_bloomberg20240814155117.csv'
+df.to_csv(output_path, index=False)
+
+print(output_path)
+
--- test0814NER_PROMPT/main.py
+++ test0814NER_PROMPT/main.py
@@ -7,27 +7,35 @@ load_dotenv(find_dotenv())
 
 # 设置你的 OpenAI API 密钥
 # openai.api_key = 'your-api-key-here'
-openai=OpenAI()
+openai = OpenAI()
 
-# 定义需要处理的文本
-text = """
-Delta Apparel Inc Apparel Inc.，拥有Beachwear品牌Salt Life和运动品牌Soffe，近日申请破产，计划出售其资产。公司已同意将Salt Life品牌出售给Forager Capital Management LLC，交易金额约为2800万美元，作为一种“跟进出价”，即设置了底价，未来有可能接到更高报价。Delta Apparel Inc.于周日向特拉华州破产法院提交了第11章申请，资产约为3.378亿美元，负债总额为2.445亿美元。公司因棉花和其他原材料价格上涨以及产品需求减少而陷入财务困境。公司股价在盘前交易中下跌最多达37%，随后暂停交易。Delta Apparel Inc.在法院保护下将继续正常运营，并已与富国银行及其他现有贷款银行达成第11章融资协议，具体融资金额尚未披露。案件编号为24-11469，案件在特拉华州破产法院审理。
-"""
 
-# 定义提取公司名称的任务
-prompt = f"请提取以下文本中的公司名称：{text} 只输出文本中的公司名称，不要输出其他不相关的内容。"
+def extract_company_names(text):
+    # 定义提取公司名称的任务
+    prompt = f"请提取以下文本中的公司名称：{text} 只输出文本中的公司名称，不要输出其他不相关的内容。如果文本中有多个公司名称，依次输出所有的公司名称，并用“、”分隔。"
 
-# 调用 GPT-4 模型
-response = openai.chat.completions.create(
-  model="gpt-4o-mini",
-  messages=[
-        {"role": "system", "content": "你是一个专业的数据标注人员。"},
-        {"role": "user", "content": prompt}
-    ]
-)
+    # 调用 GPT-4 模型
+    response = openai.chat.completions.create(
+      model="gpt-4o-mini",
+      messages=[
+            {"role": "system", "content": "你是一个专业的数据标注人员。"},
+            {"role": "user", "content": prompt}
+        ]
+    )
 
-# 获取 GPT-4 模型的回答
-company_names = response.choices[0].message.content
-res = '、'.join(company_names.split('\n'))
-print("提取出的公司名称如下：")
-print(res)
+    # 获取 GPT-4 模型的回答
+    company_names = response.choices[0].message.content
+    # res = '、'.join(company_names.split('\n'))
+    print("提取出的公司名称如下：")
+    # print(res)
+    print(company_names)
+    return company_names
+
+
+if __name__ == '__main__':
+    # 定义需要处理的文本
+    text = """
+    Delta Apparel Inc Apparel Inc.，拥有Beachwear品牌Salt Life和运动品牌Soffe，近日申请破产，计划出售其资产。公司已同意将Salt Life品牌出售给Forager Capital Management LLC，交易金额约为2800万美元，作为一种“跟进出价”，即设置了底价，未来有可能接到更高报价。Delta Apparel Inc.于周日向特拉华州破产法院提交了第11章申请，资产约为3.378亿美元，负债总额为2.445亿美元。公司因棉花和其他原材料价格上涨以及产品需求减少而陷入财务困境。公司股价在盘前交易中下跌最多达37%，随后暂停交易。Delta Apparel Inc.在法院保护下将继续正常运营，并已与富国银行及其他现有贷款银行达成第11章融资协议，具体融资金额尚未披露。案件编号为24-11469，案件在特拉华州破产法院审理。
+    """
+    extract_company_names(text)
+
